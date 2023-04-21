a75c0430ebea90e1c2fc48155442eab57f9e3ae7
anwari-fikri <anwari.fikri@gmail.com>
2023-04-21 14:44:28 +0800

    enable and disable scheduled news retrieval

--- bot/bot.py
+++ bot/bot.py
@@ -6,11 +6,15 @@ import asyncio
 
 
 class BorneoBulletinBotClient(discord.Client):
+    def __init__(self, intents=None):
+        super().__init__(intents=intents)
+        self.enabled_channels = set()
+
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
         print(f"{self.user} is now running")
         
-        self.loop.create_task(self.send_good_afternoon())
+        self.loop.create_task(self.schedule_news())
 
     async def on_message(self, message):
         if message.author.id == self.user.id:
@@ -18,30 +22,36 @@ class BorneoBulletinBotClient(discord.Client):
         
         if message.content.startswith("whats brewing today"):
             print(f"{message.author} said: {message.content}")
-            msg = await message.channel.send("Retrieving articles...")
-            responses = scraper.fake_return()
-            await msg.delete()
-            for response in responses:
-                # Create a new embed
-                embed = discord.Embed(title=response['title'], url=response['url'], description=response['content_text'][:1500], color=0x00ff00)
-                embed.set_author(name=response['author'])
-                await message.channel.send(embed=embed)
-                await asyncio.sleep(1)
+            await self.retrieve_articles(message.channel)
 
-    async def send_good_afternoon(self):
-        # Get the current time
+        if message.content == "!enable_news":
+            self.enabled_channels.add(message.channel.id)
+            await message.channel.send("News bot enabled on this channel")
+        elif message.content == "!disable_news":
+            self.enabled_channels.remove(message.channel.id)
+            await message.channel.send("News bot enabled on this channel")
+
+    async def schedule_news(self):
+        await self.wait_until_ready()
         now = datetime.datetime.now()
 
-        # If the current time is before 4:30 pm, sleep until 4:30 pm
-        if now.hour < 16 or (now.hour == 16 and now.minute < 55):
-            delta = datetime.datetime(now.year, now.month, now.day, 16, 55) - now
+        if now.hour < 14 or (now.hour == 14 and now.minute < 41):
+            delta = datetime.datetime(now.year, now.month, now.day, 14, 41) - now
             await asyncio.sleep(delta.total_seconds())
 
-        # Get the channel by ID
-        channel = self.get_channel(1097395352291770441)
+            for channel_id in self.enabled_channels:
+                channel = self.get_channel(channel_id)
+                await self.retrieve_articles(channel)
 
-        # Send the "good afternoon" message
-        await channel.send("Good afternoon!")
+    async def retrieve_articles(self, channel):
+        msg = await channel.send("Retrieving articles...")
+        responses = scraper.fake_return()
+        await msg.delete()
+        for response in responses:
+            embed = discord.Embed(title=response['title'], url=response['url'], description=response['content_text'][:1500], color=0x00ff00)
+            embed.set_author(name=response['author'])
+            await channel.send(embed=embed)
+            await asyncio.sleep(1)
 
 
 
