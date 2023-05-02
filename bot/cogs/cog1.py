f69c6b6e99ec947baa4ab1045f8486b7afe1937b
anwari-fikri <anwari.fikri@gmail.com>
2023-05-02 14:21:38 +0800

    improve performance: save today headline in json

--- bot/cogs/cog1.py
+++ bot/cogs/cog1.py
@@ -3,64 +3,98 @@ from discord.ext import commands, tasks
 from discord import app_commands
 from scraper import scraper
 import datetime
-import asyncio
+import json
 
 utc = datetime.timezone.utc
-time = datetime.time(hour=1, minute=00, tzinfo=utc) # 1am utc = 9am gmt+8 (singapore time)
+time = datetime.time(
+    hour=1, minute=0, tzinfo=utc
+)  # 1am utc = 9am gmt+8 (singapore time)
+
 
 class cog1(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
+        # TODO: set channel must be stored in configuration file or environment variable so it doesn't clear when the bot restart
         self.set_channel = []
         self.scheduled_fetch_article.start()
 
-    @app_commands.command(name="toggle_scheduled_news", description="Enable/Disable scheduled news fetching on this channel")
+    @app_commands.command(
+        name="toggle_scheduled_news",
+        description="Enable/Disable scheduled news fetching on this channel",
+    )
     async def toggle_scheduled_news(self, interaction: discord.Interaction):
         if interaction.channel_id not in self.set_channel:
             self.set_channel.append(interaction.channel_id)
-            await interaction.response.send_message(content=f"Automated News Fetching is now **ENABLED** on #{interaction.channel}")
+            await interaction.response.send_message(
+                content=f"Automated News Fetching is now **ENABLED** on #{interaction.channel}"
+            )
         elif interaction.channel_id in self.set_channel:
             self.set_channel.remove(interaction.channel_id)
-            await interaction.response.send_message(content=f"Automated News Fetching is now **DISABLED** on #{interaction.channel}")
+            await interaction.response.send_message(
+                content=f"Automated News Fetching is now **DISABLED** on #{interaction.channel}"
+            )
 
-    @app_commands.command(name="fetch_article", description="Fetch articles")
+    @app_commands.command(
+        name="fetch_article", description="Fetch articles for today's date"
+    )
     async def fetch_article(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        await interaction.followup.send("Retrieving articles. This may take up to 1 minute...")
+        await interaction.followup.send(
+            "Retrieving articles. This may take up few minutes..."
+        )
 
-        responses = scraper.main()
-        for response in responses:
-            embed = discord.Embed(
-                title=response["title"],
-                url=response["url"],
-                description=response["content_text"][:1500],
-                color=0x00FF00,
+        today_headlines = self.fetch_article_data()
+
+        for article_data in today_headlines["article_data"]:
+            await self.send_article_embed(
+                channel=interaction.channel, article_data=article_data
             )
-            embed.set_author(name=response["author"])
-            embed.set_image(url=response["image_url"])
 
-            await interaction.followup.send(embed=embed)
-            await asyncio.sleep(1)
-    
     @tasks.loop(time=time)
     async def scheduled_fetch_article(self):
         if self.set_channel != []:
             for channel_id in self.set_channel:
-                channel = await self.client.fetch_channel(channel_id)  # replace with your channel ID
-                responses = scraper.main()
-                for response in responses:
-                    embed = discord.Embed(
-                        title=response["title"],
-                        url=response["url"],
-                        description=response["content_text"][:1500],
-                        color=0x00FF00,
+                channel = await self.client.fetch_channel(channel_id)
+                today_headlines = self.fetch_article_data()
+                for article_data in today_headlines["article_data"]:
+                    await self.send_article_embed(
+                        channel=channel, article_data=article_data
                     )
-                    embed.set_author(name=response["author"])
-                    embed.set_image(url=response["image_url"])
 
-                    await channel.send(embed=embed)
-                    await asyncio.sleep(1)
+    async def send_article_embed(
+        self, channel: discord.TextChannel, article_data: dict
+    ):
+        embed = discord.Embed(
+            title=article_data["title"],
+            url=article_data["url"],
+            description=article_data["content_text"][:1500],
+            color=0x00FF00,
+        )
+        embed.set_author(name=article_data["author"])
+        embed.set_image(url=article_data["image_url"])
 
+        await channel.send(embed=embed)
+
+    def fetch_article_data(self):
+        try:
+            with open("./scraper/today_headline.json", "r") as f:
+                today_headlines = json.load(f)
+        except FileNotFoundError as e:
+            print("JSON file not found: %s", e)
+            today_headlines = {
+                "date": "",
+                "article_data": {
+                    "url": "",
+                    "title": "",
+                    "author": "",
+                    "content_text": "",
+                },
+            }
+
+        today_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        if today_headlines["date"] != today_date:
+            today_headlines = scraper.main()
+        return today_headlines
 
 
 async def setup(client: commands.Bot) -> None:
--- bot/scraper/scraper.py
+++ bot/scraper/scraper.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
+import json
 
 
 def get_today_headline(driver):
@@ -23,21 +24,32 @@ def get_today_headline(driver):
     today_date = datetime.today().strftime("%Y-%m-%d")
 
     articles = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".td_module_flex.td_module_flex_1.td_module_wrap.td-animation-stack"))
+        EC.presence_of_all_elements_located(
+            (
+                By.CSS_SELECTOR,
+                ".td_module_flex.td_module_flex_1.td_module_wrap.td-animation-stack",
+            )
+        )
     )
 
     headline_links = []
     for article in articles:
         try:
-            category = article.find_element(By.CSS_SELECTOR, ".td-post-category").text.lower()
+            category = article.find_element(
+                By.CSS_SELECTOR, ".td-post-category"
+            ).text.lower()
         except NoSuchElementException:
             category = ""
         try:
-            date = article.find_element(By.CSS_SELECTOR, ".entry-date.updated.td-module-date").get_attribute("datetime")
+            date = article.find_element(
+                By.CSS_SELECTOR, ".entry-date.updated.td-module-date"
+            ).get_attribute("datetime")
         except NoSuchElementException:
             date = ""
         if (category == "" or category == "headline") and date.startswith(today_date):
-            link = article.find_element(By.CSS_SELECTOR, ".td-image-wrap").get_attribute("href")
+            link = article.find_element(
+                By.CSS_SELECTOR, ".td-image-wrap"
+            ).get_attribute("href")
             headline_links.append(link)
 
     return headline_links
@@ -133,33 +145,44 @@ def main():
         article_data.append(get_article_data(driver, link, download_image=True))
         driver.close()
 
-    return article_data
+    today_date = datetime.today().strftime("%Y-%m-%d")
+
+    today_headline = {"date": today_date, "article_data": article_data}
+
+    with open("./scraper/today_headline.json", "w") as outfile:
+        json.dump(today_headline, outfile)
+
+    return today_headline
 
 
 def fake_return():
-    return [
-        {
-            "url": "https://borneobulletin.com.bn/raya-treats-for-the-needy/",
-            "title": "Raya treats for the needy",
-            "author": "Izah Azahari",
-            "content_text": "Network Integrity Assurance Technologies (NiAT) Sdn Bhd organised a charity shopping event titled ‘Shopping Raya with NiAT’ for 10 underprivileged families to support their Raya preparation needs yesterday. Held at the Hua Ho Department Store in OneCity Shopping Centre in Salambigar, the event was organised as part of NiAT’s corporate social responsibility (CSR) initiative this year and promote volunteerism to give back to society by instilling in its employees a caring culture that aids those who are less fortunate. During the event, NiAT covered up to BND300 worth of necessities for each of the selected family. Over 20 NiAT staff members assisted the families with their grocery purchases. NiAT’s Chief Executive Officer Lim Ming Soon said “We believe it is essential to show happiness in the lives of others, and we intend to uphold this belief through the initiative. “It gives us great delight to contribute to the happiness of these families. We are also pleased to provide them with financial assistance and bring them the joy of Hari Raya so that they, too, may enjoy the upcoming festivities as much as everyone else”. The families were identified by Projek Feed, a social enterprise focussing on aiding financially-challenged families.    ",
-            "image_url": ""
-        },
-        {
-            "url": "https://borneobulletin.com.bn/promoting-community-spirit-through-donation/",
-            "title": "Promoting community spirit through donation",
-            "author": "Azlan Othman",
-            "content_text": "Forty-four orphans of the Ministry of Religious Affairs (MoRA) workforce received donations yesterday at the ministry’s premises. Minister of Religious Affairs Pehin Udana Khatib Dato Paduka Seri Setia Ustaz Haji Awang Badaruddin bin Pengarah Dato Paduka Haji Awang Othman handed over the donations. The donations were aimed at easing the burden of the less fortunate while instilling good values among the ministry’s workforce through charity work and practices and to build a community within the ministry.  ",
-            "image_url": ""
-        },
-        {
-            "url": "https://borneobulletin.com.bn/ministry-holds-tedarus-al-quran-nationwide/",
-            "title": "Ministry holds Tedarus Al-Quran nationwide",
-            "author": "Daniel Lim & Adib Noor",
-            "content_text": "The Ministry of Culture, Youth and Sports (MCYS), through the Youth and Sports Department (JBS) held a youth Tedarus Al-Quran programme across the country to enliven the holy month of Ramadhan. In the Brunei-Muara District, the programme was held at Pintu Malim Mosque. Minister of Culture, Youth and Sports Dato Seri Setia Awang Haji Nazmi bin Haji Mohamad attended as the guest of honour. The event began with the recitation of Surah Al-Fatihah, led by the guest of honour, followed by Tedarus Al-Quran led by Pintu Malim Mosque imam Muhammad Syukri bin Pengiran Haji Sulaiman. Representatives from the Brunei History Centre, members of Bandar Seri Begawan Youth Club, Al-Busyra and alumni members of Asuhan Minda Belia Camp 2023 also participated. The ceremony continued with the recitation of Doa Tahlil, led by National Al-Quran Reading Competition for Youth 1443 Hijrah Qari runner-up Abdul Haziq Syarafuddin bin Abdul Habib. This was followed by Doa Peliharakan Sultan dan Negara Brunei Darussalam and a mass Zohor prayer.     Meanwhile, in the Temburong District, the ceremony was held at Kampong Batu Apoi, attended by Permanent Secretary (Sports) at the MCYS Pengiran Mohd Amirrizal bin Pengiran Haji Mahmud as the guest of honour. The event began with the recitation of Surah Al-Fatihah, led by the guest of honour, followed by Tedarus Al-Quran, led by Muhammad Nur Aziimin bin Shahminan from Belia Permata Hijau. This was followed by the recitation of Doa Tahlil led by Muhammad Zulkhairi bin Azamy from Belia Permata Hijau. The ceremony ended with Doa Peliharakan Sultan dan Negara Brunei Darussalam and a mass Zohor prayer led by the mosque’s imam, Shamsuddin bin Haji Azahari. In the Tutong District, the ceremony was held at Kampong Sinaut Mosque with Acting Director of Museums Pengiran Haji Rosli bin Pengiran Haji Halus attending as the guest of honour. The event began with the recitation of Surah Al-Fatihah led by the guest of honour followed by Tedarus Al-Quran led by the mosque’s imam Pengiran Muhd Fakhrin bin Pengiran Mohd Dani Muhammad. A Doa Tahlil was led by Muhammad Darwisy Afiq bin Jailani from the Tutong Youth Movement (IMPAK). The ceremony concluded with Doa Peliharakan Sultan dan Selamatkan Negara Brunei Darussalam and a mass Zohor prayer. Meanwhile, in the Belait District, the programme was held at Kampong Pandan Mosque. The event was attended by Acting Director of the National Service Division Major (Rtd) Haji Mohammad bin Dollah as the guest of honour. The programme started with the recitation of Surah Al-Fatihah led by the guest of honour, followed by the recitation of Tedarus Al-Quran, led by Haji Abdul Afiq bin Zainuddin from the Brunei Youth Council. The recitation of Doa Tahlil was led by Mohammad Nabil bin Mohammad Nordin from Religious Teachers University College of Seri Begawan (KUPU SB). The recitation of Doa Allah Peliharakan Sultan dan Negara Brunei Darussalam was led by Mosque Affairs Officer Abdul Qayyum bin Haji Aminnuddin. The guest of honour also presented a contribution from the Youth and Sports Department Belait District Branch to Abdul Qayyum. The event concluded with a mass Zohor prayer.  ",
-            "image_url": ""
-        },
-    ]
+    today_date = datetime.today().strftime("%Y-%m-%d")
+    return {
+        "date": today_date,
+        "article_data": [
+            {
+                "url": "https://borneobulletin.com.bn/raya-treats-for-the-needy/",
+                "title": "Raya treats for the needy",
+                "author": "Izah Azahari",
+                "content_text": "Network Integrity Assurance Technologies (NiAT) Sdn Bhd organised a charity shopping event titled ‘Shopping Raya with NiAT’ for 10 underprivileged families to support their Raya preparation needs yesterday. Held at the Hua Ho Department Store in OneCity Shopping Centre in Salambigar, the event was organised as part of NiAT’s corporate social responsibility (CSR) initiative this year and promote volunteerism to give back to society by instilling in its employees a caring culture that aids those who are less fortunate. During the event, NiAT covered up to BND300 worth of necessities for each of the selected family. Over 20 NiAT staff members assisted the families with their grocery purchases. NiAT’s Chief Executive Officer Lim Ming Soon said “We believe it is essential to show happiness in the lives of others, and we intend to uphold this belief through the initiative. “It gives us great delight to contribute to the happiness of these families. We are also pleased to provide them with financial assistance and bring them the joy of Hari Raya so that they, too, may enjoy the upcoming festivities as much as everyone else”. The families were identified by Projek Feed, a social enterprise focussing on aiding financially-challenged families.    ",
+                "image_url": "",
+            },
+            {
+                "url": "https://borneobulletin.com.bn/promoting-community-spirit-through-donation/",
+                "title": "Promoting community spirit through donation",
+                "author": "Azlan Othman",
+                "content_text": "Forty-four orphans of the Ministry of Religious Affairs (MoRA) workforce received donations yesterday at the ministry’s premises. Minister of Religious Affairs Pehin Udana Khatib Dato Paduka Seri Setia Ustaz Haji Awang Badaruddin bin Pengarah Dato Paduka Haji Awang Othman handed over the donations. The donations were aimed at easing the burden of the less fortunate while instilling good values among the ministry’s workforce through charity work and practices and to build a community within the ministry.  ",
+                "image_url": "",
+            },
+            {
+                "url": "https://borneobulletin.com.bn/ministry-holds-tedarus-al-quran-nationwide/",
+                "title": "Ministry holds Tedarus Al-Quran nationwide",
+                "author": "Daniel Lim & Adib Noor",
+                "content_text": "The Ministry of Culture, Youth and Sports (MCYS), through the Youth and Sports Department (JBS) held a youth Tedarus Al-Quran programme across the country to enliven the holy month of Ramadhan. In the Brunei-Muara District, the programme was held at Pintu Malim Mosque. Minister of Culture, Youth and Sports Dato Seri Setia Awang Haji Nazmi bin Haji Mohamad attended as the guest of honour. The event began with the recitation of Surah Al-Fatihah, led by the guest of honour, followed by Tedarus Al-Quran led by Pintu Malim Mosque imam Muhammad Syukri bin Pengiran Haji Sulaiman. Representatives from the Brunei History Centre, members of Bandar Seri Begawan Youth Club, Al-Busyra and alumni members of Asuhan Minda Belia Camp 2023 also participated. The ceremony continued with the recitation of Doa Tahlil, led by National Al-Quran Reading Competition for Youth 1443 Hijrah Qari runner-up Abdul Haziq Syarafuddin bin Abdul Habib. This was followed by Doa Peliharakan Sultan dan Negara Brunei Darussalam and a mass Zohor prayer.     Meanwhile, in the Temburong District, the ceremony was held at Kampong Batu Apoi, attended by Permanent Secretary (Sports) at the MCYS Pengiran Mohd Amirrizal bin Pengiran Haji Mahmud as the guest of honour. The event began with the recitation of Surah Al-Fatihah, led by the guest of honour, followed by Tedarus Al-Quran, led by Muhammad Nur Aziimin bin Shahminan from Belia Permata Hijau. This was followed by the recitation of Doa Tahlil led by Muhammad Zulkhairi bin Azamy from Belia Permata Hijau. The ceremony ended with Doa Peliharakan Sultan dan Negara Brunei Darussalam and a mass Zohor prayer led by the mosque’s imam, Shamsuddin bin Haji Azahari. In the Tutong District, the ceremony was held at Kampong Sinaut Mosque with Acting Director of Museums Pengiran Haji Rosli bin Pengiran Haji Halus attending as the guest of honour. The event began with the recitation of Surah Al-Fatihah led by the guest of honour followed by Tedarus Al-Quran led by the mosque’s imam Pengiran Muhd Fakhrin bin Pengiran Mohd Dani Muhammad. A Doa Tahlil was led by Muhammad Darwisy Afiq bin Jailani from the Tutong Youth Movement (IMPAK). The ceremony concluded with Doa Peliharakan Sultan dan Selamatkan Negara Brunei Darussalam and a mass Zohor prayer. Meanwhile, in the Belait District, the programme was held at Kampong Pandan Mosque. The event was attended by Acting Director of the National Service Division Major (Rtd) Haji Mohammad bin Dollah as the guest of honour. The programme started with the recitation of Surah Al-Fatihah led by the guest of honour, followed by the recitation of Tedarus Al-Quran, led by Haji Abdul Afiq bin Zainuddin from the Brunei Youth Council. The recitation of Doa Tahlil was led by Mohammad Nabil bin Mohammad Nordin from Religious Teachers University College of Seri Begawan (KUPU SB). The recitation of Doa Allah Peliharakan Sultan dan Negara Brunei Darussalam was led by Mosque Affairs Officer Abdul Qayyum bin Haji Aminnuddin. The guest of honour also presented a contribution from the Youth and Sports Department Belait District Branch to Abdul Qayyum. The event concluded with a mass Zohor prayer.  ",
+                "image_url": "",
+            },
+        ],
+    }
 
 
 if __name__ == "__main__":
