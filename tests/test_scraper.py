10af5a4c39e4e0d340fbd9513a2aa761096de10b
anwari-fikri <anwari.fikri@gmail.com>
2023-05-09 15:50:39 +0800

    fix pytest (real)

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -6,30 +6,37 @@ from bot.scraper import scraper
 
 
 @pytest.fixture(scope="session")
-def driver():
+def options():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(
-        options=options,
-        service=ChromeService(executable_path=ChromeDriverManager().install()),
-    )
-    yield driver
-    driver.quit()
+    return options
 
 
-def test_get_today_headline(driver):
-    result = scraper.get_today_headline(driver)
-    assert isinstance(result, list)
-    assert all(isinstance(link, str) for link in result)
-    assert all(link.startswith("https://borneobulletin.com.bn") for link in result)
+class TestScraper:
+    def test_get_today_headline(self, options):
+        driver = webdriver.Chrome(
+            options=options,
+            service=ChromeService(executable_path=ChromeDriverManager().install()),
+        )
+        result = scraper.get_today_headline(driver)
+        assert isinstance(result, list)
+        assert all(isinstance(link, str) for link in result)
+        assert all(link.startswith("https://borneobulletin.com.bn") for link in result)
+        driver.quit()
 
+    def test_get_article_data(self, options):
+        driver = webdriver.Chrome(
+            options=options,
+            service=ChromeService(executable_path=ChromeDriverManager().install()),
+        )
+        url = "https://borneobulletin.com.bn/to-the-rescue-firefighters-save-cats-from-house-fire-2/"
+        article_data = scraper.get_article_data(driver, url)
 
-def test_get_article_data(driver):
-    url = "https://borneobulletin.com.bn/to-the-rescue-firefighters-save-cats-from-house-fire-2/"
-    article_data = scraper.get_article_data(driver, url)
-
-    assert article_data["url"] == url
-    assert article_data["title"] == "To the rescue: Firefighters save cats from house fire"
-    assert article_data["author"] == "James Kon"
-    assert "Firefighters saved a number of cats while battling a two-storey house fire" in article_data["content_text"]
+        assert article_data["url"] == url
+        assert article_data["title"] == "To the rescue: Firefighters save cats from house fire"
+        assert article_data["author"] == "James Kon"
+        assert (
+            "Firefighters saved a number of cats while battling a two-storey house fire" in article_data["content_text"]
+        )
+        driver.quit()
